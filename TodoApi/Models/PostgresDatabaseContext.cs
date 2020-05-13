28a469eed2979195de8ca319dead717c73b29f11
waly <waly@waly.me>
2020-05-13 13:50:58 +0200

    Initiele setup

---
+++ TodoApi/Models/PostgresDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GMAPI.Models
+{
+    public class PostgresDatabaseContext: DbContext
+    { 
+        public PostgresDatabaseContext(DbContextOptions<PostgresDatabaseContext> options):base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using GMAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMAPI
+{
+
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+            
+
+            //configuration["hosturl"] = "https://192.168.178.50:5000";
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var dbConfig = 
+
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<PostgresDatabaseContext>(opt =>
+            {
+                opt.UseNpgsql(Configuration.GetConnectionString("MyWebApiConnection"));
+            });
+            /*services.AddDbContext<TodoContext>(opt =>
+            opt.UseInMemoryDatabase("TodoList"));*/
+            services.AddControllers();
+            //services.AddAuthorization();
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+           
+
+            app.UseForwardedHeaders();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+          
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
