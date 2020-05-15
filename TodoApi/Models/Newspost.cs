9b1dcaafd6a25188832b188a76cdd5d1430dec8a
Robin <robin.berg@hotmail.nl>
2020-05-15 12:51:24 +0200

    newspost model and controller added

---
+++ TodoApi/Models/Newspost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GMAPI.Models
+{
+    public class Newspost
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public bool Published { get; set; }
+
+        public int AccountId { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public bool Featured { get; set; }
+    }
+}
--- TodoApi/Models/PostgresDatabaseContext.cs
+++ TodoApi/Models/PostgresDatabaseContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GMAPI.Models;
 
 namespace GMAPI.Models
 {
@@ -11,5 +12,6 @@ namespace GMAPI.Models
         public PostgresDatabaseContext(DbContextOptions<PostgresDatabaseContext> options):base(options) { }
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
+        public DbSet<Newspost> Newspost { get; set; }
     }
 }
