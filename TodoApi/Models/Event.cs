88562a7f57e025ca99fcdbde65c806e9bbf5aaaf
waly <waly@waly.me>
2020-05-19 11:34:20 +0200

    Events Bootstrap

---
+++ TodoApi/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GMAPI.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string EventName { get; set; }
+
+        [Required]
+        public string EventLocationName { get; set; }
+
+        [Required]
+        public string EventLocationStreet { get; set; }
+
+        [Required]
+        public string EventLocationPostalCode { get; set; }
+
+        [Required]
+        public string EventLocationRegion { get; set; }
+
+        [Required]
+        public string EventLocationCountry { get; set; }
+
+        public Account[] EventAttendees { get; set; }
+
+        public DateTime EventDate { get; set; }
+    }
+}
