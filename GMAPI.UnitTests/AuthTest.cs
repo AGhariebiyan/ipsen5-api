72291687fd270c5ea1e4ef68161d1c5b603a18a4
Oetze van den Broek <s1102228@student.hsleiden.nl>
2020-05-28 21:29:13 +0200

    Added intergration testing and unit testing to project with a few examples

---
+++ GMAPI.UnitTests/AuthTest.cs
@@ -0,0 +1,48 @@
+﻿using GMAPI.Data;
+using GMAPI.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpsenApiTesting
+{
+    class AuthTest
+    {
+
+        private Mock<IAuthRepository> authRepoMock;
+        private Account mockAccount;
+      
+
+        [SetUp]
+        public void Setup()
+        {
+            authRepoMock = new Mock<IAuthRepository>();
+            mockAccount = new Account { 
+               Id = Guid.Parse("f2c8ba9a-2680-4584-b975-d6b44e381d08"),
+               Email = "oetze@live.nl",
+               FirstName = "Oetze",
+               MiddleName = "van den",
+               LastName = "Broek"
+            };
+            
+        }
+
+        [Test]
+        public void TestLoginFunctionality()
+        {
+            authRepoMock.Setup(a => a.Login("oetze@live.nl", "12345678")).Returns(Task.FromResult(mockAccount));
+
+            IAuthRepository authRepo = authRepoMock.Object;
+
+            Account account = authRepo.Login("oetze@live.nl", "12345678").Result;
+            
+            Assert.IsTrue(account.Email == "oetze@live.nl");
+            
+        }
+
+
+    }
+}
