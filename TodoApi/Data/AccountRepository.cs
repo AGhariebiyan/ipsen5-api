49f0d6fa618baa4138475679d99bd4b1ee5b9c59
Oetze van den Broek <s1102228@student.hsleiden.nl>
2020-06-03 20:48:35 +0200

    Fixed Merge error 2

--- TodoApi/Data/AccountRepository.cs
+++ TodoApi/Data/AccountRepository.cs
@@ -16,10 +16,6 @@ namespace GMAPI.Data
             _context = context;
         }
 
-        public async Task<Account> GetAccount(Guid id)
-        {
-            return await _context.Accounts.FirstOrDefaultAsync(a => a.Id == id);
-        }
 
         public async Task<Account> GetFullAccount(Guid id)
         {
