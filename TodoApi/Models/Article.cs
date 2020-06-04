d99508732c53d8e3e89310c68491cd06658127c7
aghariebiyan <alirezaa_afg@live.nl>
2020-06-04 11:26:46 +0200

    model variables toegevoegd

--- TodoApi/Models/Article.cs
+++ TodoApi/Models/Article.cs
@@ -8,6 +8,18 @@ namespace GMAPI.Models
 {
     public class Article
     {
-        
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Published { get; set; }
+
+        public Guid AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual Account Account { get; set; }
     }
 }
